32f53c991ff930f0aa4f48867c160be83841eb33
jdw5 <joshuadwallace1@gmail.com>
2025-08-07 22:27:35 +0000

    split: honed

--- workbench/app/Classes/Attributed.php
+++ workbench/app/Classes/Attributed.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Workbench\App\Classes;
 
 use Honed\Honed\Attributes\Experimental;
+use Honed\Honed\Attributes\Implementation;
 use Honed\Honed\Attributes\Internal;
 use Honed\Honed\Attributes\Stable;
 
 #[Stable]
 #[Experimental]
 #[Internal]
+#[Implementation]
 class Attributed
 {
     //
